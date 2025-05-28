8542b7ea2cd407e89eae97a08dd5a6fde692b526
FranciscoLi2000 <liyufeng577@gmail.com>
2025-05-28 11:41:14 +0000

    2D game

--- srcs/enemy.c
+++ srcs/enemy.c
@@ -12,4 +12,73 @@
 
 #include "so_long.h"
 
-/* 敌人相关函数 */
+/* 初始化敌人位置 */
+void	init_enemies(t_game *game)
+{
+	int	i;
+	int	j;
+	int	enemy_count;
+
+	game->map.enemy_pos = (t_pos *)malloc(sizeof(t_pos) * game->map.enemies);
+	if (!game->map.enemy_pos)
+		error_exit("Failed to allocate memory for enemies");
+	enemy_count = 0;
+	i = 0;
+	while (i < game->map.height)
+	{
+		j = 0;
+		while (j < game->map.width)
+		{
+			if (game->map.grid[i][j] == ENEMY)
+			{
+				game->map.enemy_pos[enemy_count].x = j;
+				game->map.enemy_pos[enemy_count].y = i;
+				enemy_count++;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+/* 移动单个敌人 */
+static void	move_single_enemy(t_game *game, int enemy_index)
+{
+	t_pos	*enemy;
+	int		directions[4][2] = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+	int		new_x;
+	int		new_y;
+	int		dir;
+
+	enemy = &game->map.enemy_pos[enemy_index];
+	dir = rand() % 4;
+	new_x = enemy->x + directions[dir][0];
+	new_y = enemy->y + directions[dir][1];
+	if (new_x >= 0 && new_x < game->map.width && new_y >= 0 && new_y < game->map.height
+		&& game->map.grid[new_y][new_x] != WALL
+		&& game->map.grid[new_y][new_x] != EXIT
+		&& game->map.grid[new_y][new_x] != COLLECTIBLE)
+	{
+		game->map.grid[enemy->y][enemy->x] = EMPTY;
+		game->map.grid[new_y][new_x] = ENEMY;
+		enemy->x = new_x;
+		enemy->y = new_y;
+	}
+}
+
+/* 移动所有敌人 */
+void	move_enemies(t_game *game)
+{
+	int	i;
+
+	if (game->game_state != GAME_RUNNING)
+		return ;
+	i = 0;
+	while (i < game->map.enemies)
+	{
+		move_single_enemy(game, i);
+		i++;
+	}
+	if (check_enemy_collision(game))
+		render_game(game);
+}
--- srcs/player.c
+++ srcs/player.c
@@ -12,4 +12,88 @@
 
 #include "so_long.h"
 
-/* 玩家相关函数 */
+/* 检查碰撞 */
+int	check_collision(t_game *game, int x, int y)
+{
+	if (x < 0 || x >= game->map.width || y < 0 || y >= game->map.height)
+		return (1);
+	if (game->map.grid[y][x] == WALL)
+		return (1);
+	return (0);
+}
+
+/* 收集物品 */
+void	collect_item(t_game *game, int x, int y)
+{
+	if (game->map.grid[y][x] == COLLECTIBLE)
+	{
+		game->collected++;
+		game->map.grid[y][x] = EMPTY;
+	}
+}
+
+/* 检查出口 */
+int	check_exit(t_game *game)
+{
+	int	x;
+	int	y;
+
+	x = game->map.player_pos.x;
+	y = game->map.player_pos.y;
+	if (game->map.grid[y][x] == EXIT && game->collected == game->map.collectibles)
+	{
+		game->game_state = GAME_WIN;
+		return (1);
+	}
+	return (0);
+}
+
+/* 检查敌人碰撞 */
+int	check_enemy_collision(t_game *game)
+{
+	int	i;
+	int	player_x;
+	int	player_y;
+
+	player_x = game->map.player_pos.x;
+	player_y = game->map.player_pos.y;
+	i = 0;
+	while (i < game->map.enemies)
+	{
+		if (player_x == game->map.enemy_pos[i].x && player_y == game->map.enemy_pos[i].y)
+		{
+			game->game_state = GAME_LOSE;
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+/* 移动玩家 */
+int	move_player(t_game *game, int key)
+{
+	int	new_x;
+	int	new_y;
+
+	new_x = game->map.player_pos.x;
+	new_y = game->map.player_pos.y;
+	if (key == KEY_W || key == KEY_UP)
+		new_y--;
+	else if (key == KEY_S || key == KEY_DOWN)
+		new_y++;
+	else if (key == KEY_A || key == KEY_LEFT)
+		new_x--;
+	else if (key == KEY_D || key == KEY_RIGHT)
+		new_x++;
+	if (check_collision(game, new_x, new_y))
+		return (0);
+	game->map.player_pos.x = new_x;
+	game->map.player_pos.y = new_y;
+	game->moves++;
+	collect_item(game, new_x, new_y);
+	check_exit(game);
+	check_enemy_collision(game);
+	render_game(game);
+	return (1);
+}
